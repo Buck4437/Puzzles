cb124e343d18a5a546c392b2d7169dba436d9c21
Buck4437 <Buck4437@users.noreply.github.com>
2021-06-27 23:06:45 +0800

    upload puzzle

---
+++ Puzzles/Singular/Interlocked/javascripts/index.js
@@ -0,0 +1,58 @@
+/* Heavy spoilers below */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        title: "Interlocked",
+        answer: "SYNERGISM",
+        puzzles: [
+            "Futoshiki",
+            "Jigsaw",
+            "Kakuro",
+            "Kenken",
+            "Killer",
+            "Str8ts",
+            "Sudoku (6x6)",
+            "Skyscrapers",
+            "Sudoku (9x9)"
+        ]
+    }
+})
