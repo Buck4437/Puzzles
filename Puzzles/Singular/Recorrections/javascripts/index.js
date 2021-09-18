b10318c41e9acf1bae33a250bae51bf9655005f2
Buck4437 <Buck4437@users.noreply.github.com>
2021-09-18 22:17:54 +0800

    style update

--- Puzzles/Singular/Recorrections/javascripts/index.js
+++ Puzzles/Singular/Recorrections/javascripts/index.js
@@ -7,47 +7,49 @@ var app = new Vue({
             "BLENDER": "What is the original word?",
             "ADUMBCARELESSMIXER": "You are on the right path!"
         },
-        passages:
-`As the local scientists discovered an parallel universe, they launched a lung, unending project to stripe the veil from the universe and reveal the secret off the dimensions. As the news went out, all the influencers, from fastion designers to novelists, created excitement within the public. "Realistic", baring the responsibility to lead the way, has taken the job of realizing the truth. "To infinity and beyond" is the slogans of their company. They had to unearth the truth, for else the disappointment would overwhelm them. Eventually, with the finest resign and technology, they created a teleporter…
-Score: 6
-
-
-There are sore specifc requirements that you need to complete to ranked up. Firstly, you need to kill at least 10,000 enemies win a war. Then, promote peace by allying with an neighbouring country. Afterwards, sign a traety or a contact with any countries in opposition nor war. Finally, to claim your dominance over everything, evade taxes and burn your electricity billl.
-Score: 7
-
-
-Slowly, I walk towards sum of the graves scattered around, stomping over the earfh and dust of the ancestors. In the graveyard, all bodies, rich on poor, have departed this world, with their corpses buried under the soul, the ground. "To be or not to be, what is the question." Is this where all souls are judged equally, in which money making no difference? If I die right now, all I want is to enjoy an nice meel in the underworld, leaving all the uncleen sins behind.
-Score: 4
-
-
-Beyond the chaos, we praise our lord!
-His, with his mighty power, gived us the divine gift of conscience!
-He stated de definite of good and evil, and he, the judge oof the world, separated us from chaos!
-He protected our based, our home from the evils!
-He repelled evil spirits out off our heart and soul!
-Even when the nature disasters went against us when the earth shook, the storm struck and the sea roared, our Ford still protects us from defeat!
-Beyond the chaos, we praise our lord!
-Score: 5
-
-
-You are the header oof your country. You have been at war with then neighbours for years, who are threaten your country. Therefore, it's your job to defend the country and defeat the enemies! Set up a military departure to defend yourself from attack! Send a bomb shower or a surprise presend to your opponent! Promote peace inn your own country by sending kindness, and bring terror to others by sending sum nukes to other counteries! Download the game now for free!
-Score: 8
-
-
-Within a lot of effort, I have finally implemented everythink inside the device. Project relaunch is inner sight… After checking whether all systems are gold and functional, I give my helpers the ordering to launch the device. We have moved every object, even a tiny metal barb, away from the device, for else there would be unimaginable consequences. Follower my instructions, they pressed the button, and the crystal within the device resonates without orbs around it. A large beam is then fired from the device. I have done it. With skillful and perseverance, I have relaunched the project...
-Score: 4
-
-
-WTN (World Trading Network), a highly complex nexus, allows people to trades secure through a tunnel with military-grade encryption, preventing hackers ore network service providers from collecting your precious data. An randomly generated IP address is automatically assigned to conceal your identity. Besides, you can also trade a largest amount of exotic foods, goods and products, from humans, a common manual, to dinosaurs, a legendary creature that's has not been seen for years. You might even be able to get a dead penguin when you purchase some polar bears, or even fantastic eatsu when you buy some fishes and chips!
-Score: 4
-
-
-There was a sharpen turn in the battle. The life of the girl is at steak, under the threat of the boy. In the ruins, the ground is most broken glass pieces, representing fragments of memory. The boy had founded a sharp piece of glass inner the pile of broke windows and frames. The piece is filled with the memory of pain and agony. Viciously, he threw the piece of grass at her at light speed. The piece, like a spinning blade, nor a sharp knife, tore through her clothes and into her chest. The fragment was as heavy as a rocky, launching her backwards towards a pit...
-Score: 5
-
-
-Three people, sprong and smart, had volunteered to visit the unknown. They activated the machine, and as the moter turned and the cogs rotated, a portal gradually opened. They entered their venicles, turned on the engine, and fused the portal to reach the new dimension. Upon entry, they discovered the truth of parallel dimensions: one the surface, it seems like another universe, but it is merely holes in the fabric of spacetime. All kinds of objects from the universe fall into this world: video game cartridges, tombstones, shrines, desertors, wires, dinosaurs, windows... It is like a universal dictionary. The portal is yet another tunnel two this world, which has already collapsed. They needed to find a way to escape the glitched universe. They decided to pulley bits of materials from random machines and equipments lying around this underworld and assembled them into a new teleporter. The plan was effective. They assembled the teleporter and equipped with new knowledge, escaped the glitched world.
-Score: 8
-`
+        passages: [
+            {
+                text: `As the local scientists discovered an parallel universe, they launched a lung, unending project to stripe the veil from the universe and reveal the secret off the dimensions. As the news went out, all the influencers, from fastion designers to novelists, created excitement within the public. "Realistic", baring the responsibility to lead the way, has taken the job of realizing the truth. "To infinity and beyond" is the slogans of their company. They had to unearth the truth, for else the disappointment would overwhelm them. Eventually, with the finest resign and technology, they created a teleporter...`,
+                score: 6
+            },
+            {
+                text: `There are sore specifc requirements that you need to complete to ranked up. Firstly, you need to kill at least 10,000 enemies win a war. Then, promote peace by allying with an neighbouring country. Afterwards, sign a traety or a contact with any countries in opposition nor war. Finally, to claim your dominance over everything, evade taxes and burn your electricity billl.`,
+                score: 7
+            },
+            {
+                text: `Slowly, I walk towards sum of the graves scattered around, stomping over the earfh and dust of the ancestors. In the graveyard, all bodies, rich on poor, have departed this world, with their corpses buried under the soul, the ground. "To be or not to be, what is the question." Is this where all souls are judged equally, in which money making no difference? If I die right now, all I want is to enjoy an nice meel in the underworld, leaving all the uncleen sins behind.`,
+                score: 4
+            },
+            {
+                text: `Beyond the chaos, we praise our lord!
+                His, with his mighty power, gived us the divine gift of conscience!
+                He stated de definite of good and evil, and he, the judge oof the world, separated us from chaos!
+                He protected our based, our home from the evils!
+                He repelled evil spirits out off our heart and soul!
+                Even when the nature disasters went against us when the earth shook, the storm struck and the sea roared, our Ford still protects us from defeat!
+                Beyond the chaos, we praise our lord!`,
+                score: 5
+            },
+            {
+                text: `You are the header oof your country. You have been at war with then neighbours for years, who are threaten your country. Therefore, it's your job to defend the country and defeat the enemies! Set up a military departure to defend yourself from attack! Send a bomb shower or a surprise presend to your opponent! Promote peace inn your own country by sending kindness, and bring terror to others by sending sum nukes to other counteries! Download the game now for free!`,
+                score: 8
+            },
+            {
+                text: `Within a lot of effort, I have finally implemented everythink inside the device. Project relaunch is inner sight… After checking whether all systems are gold and functional, I give my helpers the ordering to launch the device. We have moved every object, even a tiny metal barb, away from the device, for else there would be unimaginable consequences. Follower my instructions, they pressed the button, and the crystal within the device resonates without orbs around it. A large beam is then fired from the device. I have done it. With skillful and perseverance, I have relaunched the project...`,
+                score: 4
+            },
+            {
+                text: `WTN (World Trading Network), a highly complex nexus, allows people to retails items secure through a tunnel with military-grade encryption, preventing hackers ore network service providers from collecting your precious data. An randomly generated IP address is automatically assigned to conceal your identity. Besides, you can also trade a largest amount of exotic foods, goods and products, from humans, a common manual, to dinosaurs, a legendary creature that's has not been seen for years. You might even be able to get a dead penguin when you purchase some polar bears, or even fantastic eatsu when you buy some fishes and chips!`,
+                score: 4
+            },
+            {
+                text: `There was a sharpen turn in the battle. The life of the girl is at steak, under the threat of the boy. In the ruins, the ground is most broken glass pieces, representing fragments of memory. The boy had founded a sharp piece of glass inner the pile of broke windows and frames. The piece is filled with the memory of pain and agony. Viciously, he threw the piece of grass at her at light speed. The piece, like a spinning blade, nor a sharp knife, tore through her clothes and into her chest. The fragment was as heavy as a rocky, launching her backwards towards a pit...`,
+                score: 5
+            },
+            {
+                text: `Three people, sprong and smart, had volunteered to visit the unknown. They activated the machine, and as the moter turned and the cogs rotated, a portal gradually opened. They entered their venicles, turned on the engine, and fused the portal to reach the new dimension. Upon entry, they discovered the truth of parallel dimensions: one the surface, it seems like another universe, but it is merely holes in the fabric of spacetime. All kinds of objects from the universe fall into this world: video game cartridges, tombstones, shrines, desertors, wires, dinosaurs, windows... It is like a universal dictionary. The portal is yet another tunnel two this world, which has already collapsed. They needed to find a way to escape the glitched universe. They decided to pulley bits of materials from random machines and equipments lying around this underworld and assembled them into a new teleporter. The plan was effective. They assembled the teleporter and equipped with new knowledge, escaped the glitched world.`,
+                score: 8
+            }
+        ]
     }
 })
