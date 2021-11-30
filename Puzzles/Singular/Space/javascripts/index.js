67bab84a54a18998fd783ae74574be253a863df5
Buck4437 <Buck4437@users.noreply.github.com>
2021-11-30 23:36:59 +0800

    puzzle

---
+++ Puzzles/Singular/Space/javascripts/index.js
@@ -0,0 +1,54 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        title: "Space",
+        answer: "MATRIX",
+        nudge: {},
+        equations:
+`-8x+5y-z=0|9x+4y-9z=0|European venomous snake (5)
+-5x+9y-7z=-100|2x-10y+6z=56|Here we go ___... (5)
+-9x-6y+9z=-282|5x-y+7z=-44|JetBlue or Delta (7)
+-4x-9y+2z=-40|0x+5y-2z=-12|Highly impress (5)
+-5x-9y+5z=-16|3x-10y-8z=199|Tack on (6)
+-10x+0y-5z=-180|-4x+4y-3z=-90|Weapon storage (6)
+-x+4y+4z=-6|x-5y+0z=-64|Top of a house (5)
+-7x+6y-10z=8|4x+10y-3z=-145|Wire fence feature (4)
+-4x-5y-4z=119|-x+9y-10z=-61|The ___, 1958 sci-fi movie (4)
+-2x+6y+5z=-76|-x-4y-5z=72|Stall or cubicle (5)
+-10x-6y+z=-71|-8x+5y-6z=159|Bring up, raise (5)
+-3x-y+2z=-76|9x-4y+2z=86|Poise and self-respect (7)
+-9x+2y+8z=-11|4x+2y+3z=43|Shortest month (8)
+-10x-3y+8z=-223|-9x+5y+3z=-263|Boat similar to a canoe (5)
+-3x+10y+8z=-259|-2x+6y-5z=-71|Newspaper stand, e.g. (5)
+-7x-4y-10z=62|-2x+6y-5z=-48|Horizontal plane (5)
+-7x+10y+7z=-194|-3x+10y-8z=-80|Native (5)
+x+7y-10z=63|9x-10y+10z=-68|Long-headed animal (5)
+-8x+9y-9z=196|x+8y-9z=250|Thick flatbread (4)
+-5x+10y+2z=17|-3x+10y+z=50|Like very bright color (4)
+7x+10y+7z=0|10x-7y-5z=0|Strangely (5)
+x+y+6z=-16|2x+8y+8z=-82|Be against (6)
+0x-9y+6z=3|2x+3y+2z=5|Picks from a menu (6)
+x-2y+8z=-134|6x-y+z=8|Riverbank mammal (5)
+x+2y+4z=31|2x+7y-3z=155|D-Day invasion codename (8)
+-9x-10y+2z=-138|10x+4y+3z=-7|They make wool (5)
+-9x-8y-2z=-87|-x-4y-10z=29|Frosty, e.g. (6)
+-8x+6y+4z=-92|-4x-4y-3z=72|Tough-disciplined (6)
+-5x+9y+9z=-119|4x-2y-6z=76|Legendary horse (7)
+3x-6y+10z=41|9x+3y-6z=-27|Highest extent (6)`,
+        vectors:
+`-9i+9j-6k
+-8i-j+k
+-7i+7j-3k
+-3i-j-3k
+-2i-5j-10k
+-2i-j-4k
+-1i+5j+5k
+0i+2j+5k
+i+-9j+6k
+4i+4j-6k
+5i+7j+10k
+6i-10j-6k
+9i+9j+7k
+10i+4j+k`
+    }
+})
