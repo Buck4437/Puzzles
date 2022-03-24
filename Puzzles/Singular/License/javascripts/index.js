723876fedc248f7d6ad276bac9ceb05cbf57d112
Buck4437 <Buck4437@users.noreply.github.com>
2022-03-24 21:18:39 +0800

    nerf

--- Puzzles/Singular/License/javascripts/index.js
+++ Puzzles/Singular/License/javascripts/index.js
@@ -5,24 +5,24 @@ var app = new Vue({
         flavour: "Drivers like license plates that relate to themselves.",
         answer: "SERIALNUMBER",
         images: [
-            3,4,2,3,3,
-            2,2,2,2,4,
-            2,2,3,1,1
+            2,4,1,2,2,
+            4,2,3,3,2,
+            2,3,3,2,2
         ],
-        plates:`S954123
-H3457
-D15312
-C41260
-S11443
-C41253
-H31272
-N3571241
-F1055
-P147312
-H44444312
-K3848
-J35531
+        plates:`45731215K
+37H3123U44
+P1473
+812455
+D123444
+F145H116H
+F1047
+571N6
+1U448312
+D12460N
+044364
+C08417
+73135C0P3
 X1247
-D1246`.split("\n")
+6124V177`.split("\n")
     }
 });
