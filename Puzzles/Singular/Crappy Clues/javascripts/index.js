b8da28a9ee7f5eb091ff2c9e6ceb4f5265139ba0
Buck4437 <Buck4437@users.noreply.github.com>
2022-04-18 21:14:23 +0800

    a

--- Puzzles/Singular/Crappy Clues/javascripts/index.js
+++ Puzzles/Singular/Crappy Clues/javascripts/index.js
@@ -5,18 +5,36 @@ var app = new Vue({
         flavour: "Charlie has created a couple of cryptic, yet crappy clues.",
         answer: "CHAOS",
         clues:
-`6: Bread sauces containers contain some flavourings (6)
-5: Kind of a rbchi tree (5)
-10: Even aabcc oddea -Conclusion- (4)
-7: Area, Absolute, and RAC in TT are theoretical (8)
-9: Clay from crushed talc (4)
-3: Desc. error: "---> New <--- testament smell" (5)
-4: Daughter (Acttwo): flipped, reversed, and confrontation (3,3)
-8: Qwertyuiopasdb aco nbvcxzlkjhgfds contains meat (5)
-2: Initials of a crazily-easily-and-stupidly-enumerated halt (5)
-1: (New) a cat! (3)
-12: Synchronize a fall echo (4)
-1: <br> <a> Cod confuses snake (5)
-11: A 0.02777777777777777777777777777... yard move (4)`.split("\n")
+`6. Bread sauces containers contain some flavourings (6)
+5. Kind of a rbchi tree (5)
+10. Even aabcc oddea -Conclusion- (4)
+7. Area, Absolute, and RAC in TT are theoretical (8)
+9. Clay from crushed talc (4)
+3. Desc. error: "---> New <--- testament smell" (5)
+4. Daughter (Acttwo): flipped, reversed, and confrontation (3,3)
+8. Qwertyuiopasdb aco nbvcxzlkjhgfds contains meat (5)
+2. Initials of a crazily-easily-and-stupidly-enumerated halt (5)
+1. (New) a cat! (3)
+12. Synchronize a fall echo (4)
+1. <br> <a> Cod confuses snake (5)
+11. A 0.02777777777777777777777777777... yard move (4)`.split("\n")
     }
 });
+
+
+/*
+Alt clues:
+6: Bread sauces containers contain some flavourings (6)
+5: Out of place rbchi tree (5)
+10: Even acbo cdda -Conclusion- (4)
+7: Absolute, R and AC in TT are theoretical (8)
+9: Clay from crushed talc (4)
+3: Esc. error: "New testament smell" (5)
+4: Confrontation (acttwo): flipped, reversed, back, ascended (3, 3)
+8 :Qwertyuiopasb aco nbvcxzlkjhgfds contains meat (5) (5)
+2: Initials of crazily-easily-and-stupidly-enumerated to halt (5)
+1: (New) cat cat (3)
+12: Synchronize to drop echo (4)
+1: <br> <a> Co. confuses snake (5)
+11: A 0.02777777777777777777777777777... yard move (4)
+*/
