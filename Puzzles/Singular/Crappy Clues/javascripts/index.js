7c9563219de082ed32207dfc9cc2d19596ac9c51
Buck4437 <Buck4437@users.noreply.github.com>
2022-04-18 11:13:29 +0800

    new horrible puzzle

---
+++ Puzzles/Singular/Crappy Clues/javascripts/index.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        title: "Crappy Clues",
+        flavour: "Charlie has created a couple of cryptic, yet crappy clues.",
+        answer: "CHAOS",
+        clues:
+`Bread sauces container contain some flavourings (6)
+Kind of a rbchi tree (5)
+Even aabcc oddea -Conclusion- (4)
+Area, Absolute, and RAC in TT are theoretical (8)
+Clay in crushed talc (4)
+Desc. error: "New testament smell" (5)
+Daughter (Acttwo): flipped, reversed, and confrontation! (6)
+Qwertyuippasdfgh jklzxcvbnmdbaco nbvcxzlkjhgfds contains meat (5)
+Initials of a crazily-easily-and-stupidly-enumerated halt (5)
+(New) a cat! (3)
+Synchronization - a capsize echo (4)
+<br> <a> Cod confuses snake (5)
+A 0.02777777777777777777777777777... yard move (4)`.split("\n")
+    }
+});
