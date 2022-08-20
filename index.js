7852728ac16f846cd6c316e87585d587ff465932
Buck4437 <Buck4437@users.noreply.github.com>
2022-08-20 14:58:24 +0800

    added home page

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        singulars: [
+            "Matrix",
+            "Binder",
+            "Interlocked",
+            "Recorrections",
+            "Space",
+            "License",
+            "Crappy Clues"
+        ]
+    }
+})
