5216e37afe77029aba7f67872717e7e5e6b2604b
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2023-06-20 14:03:08 +0800

    consistency fix

--- Hunts/June2023/puzzle1/javascripts/index.js
+++ Hunts/June2023/puzzle1/javascripts/index.js
@@ -10,7 +10,7 @@ var app = new Vue({
         Start at Ginza. Ride 3 stops to a major interchange. Read the second letter.
         Start at the busiest station. Alight at platform with number greater than 10. Ride 15 stops. Read the third letter.
         Start at the busiest station. Ride 1 stop to a station that starts with a T.  Exit the train. Enter the train. Ride 38 stops. Read the seventh letter.
-        Start at the station whose translated name is "Avenue of the Americas". Ride 14 stops. Read the 6th letter.
+        Start at the station whose translated name is "Avenue of the Americas". Ride 14 stops. Read the sixth letter.
         Start at the busiest station in Korea. Ride 11 stops. Exit the train. Enter the train. Ride to the terminal. Read the sixth letter.
         Start at the station in Thailand with a prime number in its name. Ride 1 stop. Exit the train. Enter the train. Ride to the terminal. Read the fifth letter.
         Start at the station with Belarusian themes. Ride 36 stops. Read the third letter.`.split("\n").map(x => x.trim())
