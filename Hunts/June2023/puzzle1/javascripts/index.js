2ef007b7edad002ca78a0f94f79def60255e08b0
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2023-06-20 14:01:39 +0800

    Fix clue

--- Hunts/June2023/puzzle1/javascripts/index.js
+++ Hunts/June2023/puzzle1/javascripts/index.js
@@ -9,7 +9,7 @@ var app = new Vue({
         Start at the station in Ukraine constructed in 1927-1932. Ride 15 stops. Read the fourth letter.
         Start at Ginza. Ride 3 stops to a major interchange. Read the second letter.
         Start at the busiest station. Alight at platform with number greater than 10. Ride 15 stops. Read the third letter.
-        Start at the busiest station. Ride 1 stop to a station that starts with a T.  Exit the train. Enter the train. Ride 38 stops. Read the ninth letter.
+        Start at the busiest station. Ride 1 stop to a station that starts with a T.  Exit the train. Enter the train. Ride 38 stops. Read the seventh letter.
         Start at the station whose translated name is "Avenue of the Americas". Ride 14 stops. Read the 6th letter.
         Start at the busiest station in Korea. Ride 11 stops. Exit the train. Enter the train. Ride to the terminal. Read the sixth letter.
         Start at the station in Thailand with a prime number in its name. Ride 1 stop. Exit the train. Enter the train. Ride to the terminal. Read the fifth letter.
