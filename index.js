ac783b86535853bc53f4ddba76ed841eff84ac9d
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2026-02-16 12:23:08 +0800

    added puzzles

--- index.js
+++ index.js
@@ -40,6 +40,57 @@ const app = new Vue({
                 desc: "2023 Sep, originally written for a mini-hunt",
                 link: "A-logic-puzzle"
             }
+        ],
+        hunts: [
+            {
+                name: "MIT Mystery Hunt 2026",
+                puzzles: [
+                    {
+                        name: "Jumping to Conclusions",
+                        link: "https://puzzmon.world/puzzles/jumping_to_conclusions",
+                    },
+                    {
+                        name: "Jam Session",
+                        link: "https://puzzmon.world/puzzles/jam_session",
+                    },
+                    {
+                        name: "Stray Child",
+                        link: "https://puzzmon.world/puzzles/stray_child",
+                    },
+                    {
+                        name: "Hear Me Out",
+                        link: "https://puzzmon.world/puzzles/hear_me_out",
+                    },
+                    {
+                        name: "Goku",
+                        link: "https://puzzmon.world/puzzles/goku",
+                    },
+                    {
+                        name: "Layers: Bordered",
+                        link: "https://puzzmon.world/puzzles/layers_bordered",
+                    },
+                    {
+                        name: "Layers: Bowled",
+                        link: "https://puzzmon.world/puzzles/layers_bowled",
+                    },
+                    {
+                        name: "Layers: Colored",
+                        link: "https://puzzmon.world/puzzles/layers_colored",
+                    },
+                    {
+                        name: "Layers: Rounded",
+                        link: "https://puzzmon.world/puzzles/layers_rounded",
+                    },
+                    {
+                        name: "Layers: Trapezoidal",
+                        link: "https://puzzmon.world/puzzles/layers_trapezoidal",
+                    },
+                    {
+                        name: "Layers",
+                        link: "https://puzzmon.world/puzzles/layers",
+                    },
+                ]
+            }
         ]
     }
 })
